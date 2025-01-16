8bab778d20a060eebc845157e02dcc9d319d98ca
gasfet <49603992+gasfet@users.noreply.github.com>
2025-01-16 13:21:32 +0900

    작업 내용 update

--- src/pjts/makeMatlabInterface/makeInterface.m
+++ src/pjts/makeMatlabInterface/makeInterface.m
@@ -1,16 +1,21 @@
 %%
 
 % hFile = ["ftd2xx.h", "ftdi_common.h", "ftdi_infra.h", "ftdi_mid.h", "libmpsse_i2c.h", "libmpsse_spi.h"];
-hFile = [ "libmpsse_i2c.h", "libmpsse_spi.h"];
+% hFile = [ "libmpsse_i2c.h", "libmpsse_spi.h"];
+hFile = ["all.h"];
 
 clibgen.generateLibraryDefinition( hFile, ...
 Libraries="libmpsse.lib", ...
 IncludePath=pwd(), ...
 InterfaceName="ftLib", ...
 OutputFolder=pwd(), ...
-DefinedMacros=["FT_VER_MAJOR=1","FT_VER_MINOR=0", "FT_VER_BUILD=1", "WIN32", "_WINDOWS", "_USRDLL", "FTDIMPSSE_EXPORTS"], ...
 OverwriteExistingDefinitionFiles=true);
 
 
 %%
+
+% unload(clibConfiguration('ftLib'))
+
 build(defineftLib)
+addpath("ftLib\");
+clibConfiguration('ftLib','ExecutionMode','outofprocess')
